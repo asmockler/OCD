8f8ad0ab779045146d844ce7f7f3578e10f546e6
asmockler <asmockler@gmail.com>
2016-01-09 20:15:21 -0700

    Initial Commit. Working scrolling view and dismissable text

---
+++ app/styles/text.js
@@ -0,0 +1,14 @@
+'use strict';
+var React = require('react-native');
+var {
+  StyleSheet,
+} = React;
+
+var text = StyleSheet.create({
+  movingText: {
+    fontSize: 50,
+    textAlign: 'center'
+  }
+})
+
+module.exports = text;
