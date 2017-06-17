ec8162073240098da6b9f3bebe33c365bbe5d118
Andy Mockler <asmockler@gmail.com>
2017-06-16 19:29:46 -0500

    Reimplement tap to start label

---
+++ OCD/OCD/UILabel+setKerning.swift
@@ -0,0 +1,19 @@
+//
+//  UILabel+setKerning.swift
+//  OCD
+//
+//  Created by Andy Mockler on 6/16/17.
+//  Copyright © 2017 Andy Mockler. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    func setKerning(amount: CGFloat) {
+        if text != nil {
+            let attributedString = NSMutableAttributedString(string: text!)
+            attributedString.addAttribute(NSKernAttributeName, value: amount, range: NSRange(location: 0, length: attributedString.length - 1))
+            attributedText = attributedString
+        }
+    }
+}
